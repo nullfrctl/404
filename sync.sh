754aff72195a9c5e9597e245ae75833ff448b3d9
nullfrctl <nullfrctl@protonmail.com>
2022-07-14 17:27:22 -0600

    remove sync.zsh, obsoleted by sync.sh

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ln -sf ./config/  "$HOME/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/overrides"
-ln -sf ./custom/* "$HOME/.local/share/Steam/steamapps/common/Team Fortress 2/tf/custom/"
-
-# remove weird "config" symlink inside of config (recursive symlink?)
-rm -rf ./config/config
