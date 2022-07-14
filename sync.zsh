bd3439ea495b2bef9844c147de05dc6e78a3ca60
nullfrctl <nullfrctl@protonmail.com>
2022-07-14 17:30:45 -0600

    finally finalize sync script transition

--- sync.zsh
+++
@@ -1,5 +0,0 @@
-ln -sf $HOME/.config/tf2/config/  "$HOME/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/overrides"
-ln -sf $HOME/.config/tf2/custom/* "$HOME/.local/share/Steam/steamapps/common/Team Fortress 2/tf/custom/"
-
-# remove weird "config" symlink inside of config (recursive symlink?)
-rm -rf $HOME/.config/tf2/config/config
